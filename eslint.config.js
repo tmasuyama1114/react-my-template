2acf185acd1aa2a13770cc326eb3f8ea0748f876
tmasuyama1114 <tmasuyama1114@gmail.com>
2024-11-03 07:50:56 +0900

    delete eslintrc.cjs

--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,6 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       '@typescript-eslint/no-explicit-any': 'error',
-      'no-unused-vars': 'warn',
     },
   }
 )
